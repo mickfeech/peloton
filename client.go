18aba3260313d2d1a5c5ad61954f2348fa204900
mcfeech <cmcfee@kent.edu>
2019-06-07 16:13:36 -0400

    restructure client to persist cookie in jar across several requests

--- client.go
+++ client.go
@@ -1,9 +1,12 @@
 package peloton
 
 import (
+	"bytes"
 	"crypto/tls"
 	"encoding/json"
 	"net/http"
+	"net/http/cookiejar"
+	"time"
 )
 
 // BaseAddress is the api endpoint for peloton
@@ -13,33 +16,42 @@ const BaseAddress = "https://api.onepeloton.com/api"
 type Client struct {
 	Client  *http.Client
 	baseURL string
+	jar     http.CookieJar
 }
 
 // NewClient initializes a new HTTP client to interact with the web api
 func NewClient() *Client {
+	j, _ := cookiejar.New(nil)
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	jClient := &http.Client{Jar: j, Transport: transCfg, Timeout: time.Second * 10}
+
 	return &Client{
-		Client:  http.DefaultClient,
+		Client:  jClient,
 		baseURL: BaseAddress,
 	}
 }
 
 // Get executes a HTTP request to the web api
 func (c *Client) Get(url string, result interface{}) error {
-	for {
-		transCfg := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		c.Client = &http.Client{Transport: transCfg}
-		resp, err := c.Client.Get(url)
-		defer resp.Body.Close()
-		if err != nil {
-			return err
-		}
-		err = json.NewDecoder(resp.Body).Decode(result)
-		if err != nil {
-			return err
-		}
-		break
+	resp, err := c.Client.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+	err = json.NewDecoder(resp.Body).Decode(result)
+	if err != nil {
+		return err
 	}
 	return nil
+}
+
+// GetCookie executes a request to set the header to maintain session with the api
+func (c *Client) GetCookie(apiUrl string, data []byte) []*http.Cookie {
+	req, _ := http.NewRequest("POST", apiUrl, bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := c.Client.Do(req)
+	defer resp.Body.Close()
+	return resp.Cookies()
 }
