95e6c7c22dbfa72bf69317352334d0914b6225a4
mcfeech <cmcfee@kent.edu>
2019-06-07 09:44:57 -0400

    Add comments and change get to Get

--- client.go
+++ client.go
@@ -6,13 +6,16 @@ import (
 	"net/http"
 )
 
+// BaseAddress is the api endpoint for peloton
 const BaseAddress = "https://api.onepeloton.com/api"
 
+// Client is a client for working with the peloton web api
 type Client struct {
 	Client  *http.Client
 	baseURL string
 }
 
+// NewClient initializes a new HTTP client to interact with the web api
 func NewClient() *Client {
 	return &Client{
 		Client:  http.DefaultClient,
@@ -20,17 +23,18 @@ func NewClient() *Client {
 	}
 }
 
-func (c *Client) get(url string, result interface{}) error {
+// Get executes a HTTP request to the web api
+func (c *Client) Get(url string, result interface{}) error {
 	for {
 		transCfg := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
 		c.Client = &http.Client{Transport: transCfg}
 		resp, err := c.Client.Get(url)
+		defer resp.Body.Close()
 		if err != nil {
 			return err
 		}
-		defer resp.Body.Close()
 		err = json.NewDecoder(resp.Body).Decode(result)
 		if err != nil {
 			return err
