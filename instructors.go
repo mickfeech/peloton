e391ca12ddcd9ee0d6107c6217d1efa92047ec66
mcfeech <cmcfee@kent.edu>
2019-06-07 09:42:41 -0400

    Add comments and change get to Get

--- instructors.go
+++ instructors.go
@@ -1,9 +1,11 @@
 package peloton
 
+// InstructorsResponse contains data element from initial call to the api
 type InstructorsResponse struct {
 	Data []Instructor `json:"data"`
 }
 
+// Instructor contains basic information about instructors
 type Instructor struct {
 	ID              string   `json:"id"`
 	FirstName       string   `json:"first_name"`
@@ -13,10 +15,11 @@ type Instructor struct {
 	SpotifyPlaylist string   `json:"spotify_playlist_uri"`
 }
 
+// GetInstructors gets instructor information
 func (c *Client) GetInstructors() (*InstructorsResponse, error) {
 	pelotonURL := c.baseURL + "/instructor"
 	var t InstructorsResponse
-	err := c.get(pelotonURL, &t)
+	err := c.Get(pelotonURL, &t)
 	if err != nil {
 		return nil, err
 	}
