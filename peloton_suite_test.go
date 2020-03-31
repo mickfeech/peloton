2db02d37f149a7afa61e43ce328a4bc932ed45ab
mickfeech <cmcfee@kent.edu>
2020-03-31 14:42:43 -0400

    Restructure test files

--- peloton_suite_test.go
+++ peloton_suite_test.go
@@ -1,12 +1,25 @@
 package peloton_test
 
 import (
+	"github.com/mickfeech/peloton"
+	"gopkg.in/h2non/gock.v1"
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
-func TestClient(t *testing.T) {
+var client *peloton.ApiClient
+
+func TestPeloton(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Client Suite")
+	RunSpecs(t, "Peloton Suite")
 }
+
+var _ = BeforeSuite(func() {
+	client = peloton.NewApiClient("", "")
+})
+
+var _ = BeforeEach(func() {
+	defer gock.Off()
+})
