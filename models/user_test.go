94b11795eb4933064b9f6c6ded378406db4759a7
mickfeech <cmcfee@kent.edu>
2020-04-01 09:26:46 -0400

    Add more attribute tests

--- models/user_test.go
+++ models/user_test.go
@@ -24,4 +24,37 @@ var _ = Describe("Me Model", func() {
 	It("Has a FirstName attribute", func() {
 		Expect(me.FirstName).To(Equal("John"))
 	})
+	It("Has a FirstName attribute", func() {
+		Expect(me.LastName).To(Equal("Doe"))
+	})
+	It("Has a Username attribute", func() {
+		Expect(me.Username).To(Equal("jdoe"))
+	})
+	It("Has a Birthday attribute", func() {
+		Expect(me.Birthday).To(Equal(323358625))
+	})
+	It("Has a Ftp attribute", func() {
+		Expect(me.Ftp).To(Equal(153))
+	})
+	It("Has a Gender attribute", func() {
+		Expect(me.Gender).To(Equal("male"))
+	})
+	It("Has a Location attribute", func() {
+		Expect(me.Location).To(Equal("Nowhere USA"))
+	})
+	It("Has a Weight attribute", func() {
+		Expect(me.Weight).To(Equal(192.0))
+	})
+	It("Has a Height attribute", func() {
+		Expect(me.Height).To(Equal(71.0))
+	})
+	It("Has a TotalWorkOuts attribute", func() {
+		Expect(me.TotalWorkOuts).To(Equal(483))
+	})
+	It("Has a DefaultMaxHr attribute", func() {
+		Expect(me.DefaultMaxHr).To(Equal(183))
+	})
+	It("Has a CustomMaxHr attribute", func() {
+		Expect(me.CustomMaxHr).To(Equal(203))
+	})
 })
