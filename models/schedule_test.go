f734865d42f0c6c699d904e9012a7568fc2731f2
mickfeech <cmcfee@kent.edu>
2020-05-05 12:59:41 -0400

    Add testing

--- models/schedule_test.go
+++ models/schedule_test.go
@@ -25,4 +25,40 @@ var _ = Describe("Schedule Model", func() {
 	It("Has a Total attribute", func() {
 		Expect(mySchedule.Total).To(Equal(4))
 	})
+	It("Has a data attribute", func() {
+		Expect(mySchedule.Data).NotTo(Equal(nil))
+	})
+	It("Has an Instructors attribute", func() {
+		Expect(mySchedule.Instructors).NotTo(Equal(nil))
+	})
+	It("Has an RideTypes attribute", func() {
+		Expect(mySchedule.RideTypes).NotTo(Equal(nil))
+	})
+	It("Has an ID attribute", func() {
+		Expect(mySchedule.Data[0].ID).To(Equal("d7fca24717014685aa9461d8b64e147a"))
+	})
+	It("Has a RideID attribute", func() {
+		Expect(mySchedule.Data[0].RideID).To(Equal("6228e8bfda184914b38138cd8ea463d1"))
+	})
+	It("Has an StartTime attribute", func() {
+		Expect(mySchedule.Data[0].StartTime).To(Equal(1588690800))
+	})
+	It("Has an EndTime attribute", func() {
+		Expect(mySchedule.Data[0].EndTime).To(Equal(1588692772))
+	})
+	It("Has an Status attribute", func() {
+		Expect(mySchedule.Data[0].Status).To(Equal("PRE_START"))
+	})
+	It("Has an IsLive attribute", func() {
+		Expect(mySchedule.Data[0].IsLive).To(Equal(true))
+	})
+	It("Has an IsStudio attribute", func() {
+		Expect(mySchedule.Data[0].IsStudio).To(Equal(false))
+	})
+	It("Has an IsEncore attribute", func() {
+		Expect(mySchedule.Data[0].IsEncore).To(Equal(true))
+	})
+	It("Has an TotalHomeReservations attribute", func() {
+		Expect(mySchedule.Data[0].TotalHomeReservations).To(Equal(395))
+	})
 })
--- schedule_test.go
+++ schedule_test.go
@@ -1,12 +1,31 @@
 package peloton_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/mickfeech/peloton"
+	"gopkg.in/h2non/gock.v1"
+	"io/ioutil"
 )
 
-var _ = Describe("Schedule", func() {
-
+var _ = Describe("Schedules Models", func() {
+	BeforeEach(func() {
+		testData, err := ioutil.ReadFile("testdata/schedule.json")
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+			Fail("Error retrieving json test data")
+		}
+		gock.New("https://api.onepeloton.com/api/v3/ride/live").
+			Reply(200).
+			JSON(testData)
+		gock.InterceptClient(client.Client.GetClient())
+	})
+	It("Should not return an error", func() {
+		_, err := client.GetSchedule(1588680000, 1588698000)
+		Expect(err).To(BeNil())
+	})
+	It("Should not return an error", func() {
+		m, _ := client.GetSchedule(1588680000, 1588698000)
+		Expect(m.Count).To(Equal(4))
+	})
 })
