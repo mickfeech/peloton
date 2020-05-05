20ff40134af86fa3509f2a6902630acb34d50d91
mickfeech <cmcfee@kent.edu>
2020-05-05 10:18:17 -0400

    Move api calls

--- client.go
+++ client.go
@@ -8,7 +8,6 @@ package peloton
 import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
-	"github.com/mickfeech/peloton/models"
 	"log"
 	"net/http"
 	"os"
@@ -74,42 +73,4 @@ func (c *ApiClient) UpdateAuthCookie() (*http.Cookie, error) {
 	cookie, _ := GetInitialAuthCookie(c.Client, c.Username, c.Password)
 	c.Cookie = cookie
 	return cookie, nil
-}
-
-// Me method creates a request to retrieve data about the current user
-func (c *ApiClient) Me() (*models.User, error) {
-	resp, _ := c.Client.R().
-		SetHeader("Accept", "application/json").
-		SetResult(&models.User{}).
-		Get("/api/me")
-	return resp.Result().(*models.User), nil
-}
-
-// Instructors method creates a request to retrieve data about the instructors
-func (c *ApiClient) GetInstructors() (*models.Instructors, error) {
-	resp, _ := c.Client.R().
-		SetHeader("Accept", "application/json").
-		SetResult(&models.Instructors{}).
-		Get("/api/instructor")
-	return resp.Result().(*models.Instructors), nil
-}
-
-// Workouts method creates a request to retrieve workout data by userid
-func (c *ApiClient) GetUserWorkouts(userid string) (*models.Workouts, error) {
-	userWorkoutUrl := fmt.Sprintf("/api/user/%s/workouts", userid)
-	resp, _ := c.Client.R().
-		SetHeader("Accept", "application/json").
-		SetResult(&models.Workouts{}).
-		Get(userWorkoutUrl)
-	return resp.Result().(*models.Workouts), nil
-}
-
-// Get specific workout details
-func (c *ApiClient) GetWorkoutDetails(workoutId string, interval string) (*models.WorkOutDetails, error) {
-	workoutDetailUrl := fmt.Sprintf("/api/workout/%s/performance_graph?every_n=%s", workoutId, interval)
-	resp, _ := c.Client.R().
-		SetHeader("Accept", "application/json").
-		SetResult(&models.WorkOutDetails{}).
-		Get(workoutDetailUrl)
-	return resp.Result().(*models.WorkOutDetails), nil
 }
